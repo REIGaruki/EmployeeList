6f0432abb70bc68d1fe66ceaf9a972470a3b779c
REIGaruki <gyakujo@mail.ru>
2024-05-15 17:12:31 +0300

    New class EmployeeService

---
+++ src/main/java/pro/sky/EmployeeList/EmployeeService.java
@@ -0,0 +1,10 @@
+package pro.sky.EmployeeList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeService {
+    List<Employee> employees = new ArrayList<>();
+    private final int MAX_EMPLOYEE_QUANTITY = 100;
+
+}
