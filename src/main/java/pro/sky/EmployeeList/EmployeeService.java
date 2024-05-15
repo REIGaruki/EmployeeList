4a96bc0bea8bc1a23625ac78c058f36669fd88b5
REIGaruki <gyakujo@mail.ru>
2024-05-15 17:29:16 +0300

    New EmployeeService methods

--- src/main/java/pro/sky/EmployeeList/EmployeeService.java
+++ src/main/java/pro/sky/EmployeeList/EmployeeService.java
@@ -1,10 +1,33 @@
 package pro.sky.EmployeeList;
 
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class EmployeeService {
-    List<Employee> employees = new ArrayList<>();
-    private final int MAX_EMPLOYEE_QUANTITY = 100;
+    List<Employee> employees = new ArrayList<Employee>();
+    private final int MAX_EMPLOYEE_QUANTITY = 5;
+
+    public void addEmployee(String firstName, String lastName) {
+        employees.add(new Employee(firstName, lastName));
+    }
+    public void removeEmployee(String firstName, String lastName) {
+        for (Employee employee : employees) {
+            if (employee.getFirstName().equalsIgnoreCase(firstName) &&
+                    employee.getLastName().equalsIgnoreCase(lastName)) {
+                employees.remove(employee);
+            }
+        }
+    }
+    public String findEmployee(String firstName, String lastName) {
+        for (Employee employee : employees) {
+            if (employee.getFirstName().equalsIgnoreCase(firstName) &&
+                    employee.getLastName().equalsIgnoreCase(lastName)) {
+                return employee.toString();
+            }
+        }
+    }
 
 }
