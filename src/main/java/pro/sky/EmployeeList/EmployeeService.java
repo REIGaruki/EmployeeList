62d448eea8c7c48a39318d4632f5f3b76e58436f
REIGaruki <gyakujo@mail.ru>
2024-05-15 18:07:05 +0300

    New EmployeeStorageIsFull Exception

--- src/main/java/pro/sky/EmployeeList/EmployeeService.java
+++ src/main/java/pro/sky/EmployeeList/EmployeeService.java
@@ -10,28 +10,45 @@ public class EmployeeService {
     List<Employee> employees = new ArrayList<Employee>();
     private final int MAX_EMPLOYEE_QUANTITY = 5;
 
-    public void addEmployee(String firstName, String lastName) {
-        employees.add(new Employee(firstName, lastName));
-    }
-    public void removeEmployee(String firstName, String lastName) {
-        for (Employee employee : employees) {
-            if (employee.getFirstName().equalsIgnoreCase(firstName) &&
-                    employee.getLastName().equalsIgnoreCase(lastName)) {
-                employees.remove(employee);
+    public String addEmployee(String firstName, String lastName) {
+        try {
+            if (employees.size() >= MAX_EMPLOYEE_QUANTITY) {
+                throw new EmployeeStorageIsFullException("Employee Storage Is Full");
+            } else {
+                employees.add(new Employee(firstName, lastName));
+                return "Employee added";
             }
+        } catch (EmployeeNotFoundException e) {
+            return e.getMessage();
+        }
+    }
+    public String removeEmployee(String firstName, String lastName) {
+        try {
+            Employee employee = findByName(firstName, lastName);
+            employees.remove(employee);
+            return "Employee removed";
+        } catch (NullPointerException e) {
+            return "Can't remove, employee does not exist";
         }
     }
     public String findEmployee(String firstName, String lastName) {
         try {
+            return findByName(firstName, lastName).toString();
+        } catch(NullPointerException e) {
+            return "Employee is not found";
+        }
+    }
+    public Employee findByName(String firstName, String lastName) {
+        try {
             for (Employee employee : employees) {
                 if (employee.getFirstName().equalsIgnoreCase(firstName) &&
                         employee.getLastName().equalsIgnoreCase(lastName)) {
-                    return employee.toString();
+                    return employee;
                 }
             }
             throw new EmployeeNotFoundException("No such employee exists");
         } catch (EmployeeNotFoundException e) {
-            return e.getMessage();
+            return null;
         }
     }
 
