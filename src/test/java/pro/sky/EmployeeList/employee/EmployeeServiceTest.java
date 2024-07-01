35ba67cff825a55aa961b3ed23b0512933930e99
REIGaruki <gyakujo@mail.ru>
2024-07-01 14:23:18 +0300

    JUnit Initial

---
+++ src/test/java/pro/sky/EmployeeList/employee/EmployeeServiceTest.java
@@ -0,0 +1,40 @@
+package pro.sky.EmployeeList.employee;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeServiceTest {
+    @BeforeEach
+
+
+    @Test
+    void printEmployees() {
+    }
+
+    @Test
+    void addEmployee() {
+    }
+    @Test
+    void addEmployeeThatAlreadyExist() {
+    }
+    @Test
+    void addEmployeeWithNoEmptySpace() {
+    }
+
+    @Test
+    void removeEmployee() {
+    }
+
+    @Test
+    void findEmployee() {
+    }
+    @Test
+    void removeEmployeeThatNotExist() {
+    }
+
+    @Test
+    void findEmployeeThatNotExist() {
+    }
+}
